ac9f0c29167715729696c9d9fefb3896bee8b1a5
Connor Duncan <connorduncan@cs.ucsb.edu>
2023-12-26 15:51:47 -0800

    longestCommonPrefix

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,45 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        java.lang.String romanNumerals = "LVIII";
-        int sum = 0;
-        System.out.println("Hello world!");
-        HashMap<Character, Integer> myMap = new HashMap<Character, Integer>();
-        myMap.put('M', 1000);
-        myMap.put('D', 500);
-        myMap.put('C', 100);
-        myMap.put('L', 50);
-        myMap.put('X', 10);
-        myMap.put('V', 5);
-        myMap.put('I', 1);
-        int i = 0;
-        for(i = 0; i < romanNumerals.length() - 1; i++){
-            if(myMap.get(romanNumerals.charAt(i)) < myMap.get(romanNumerals.charAt(i+1))){
-                sum += myMap.get(romanNumerals.charAt(i+1)) - myMap.get(romanNumerals.charAt(i));
-                i++;
-            }
-            else{
-                sum += myMap.get(romanNumerals.charAt(i));
-            }
-        }
-        if(i < romanNumerals.length()){
-            sum += myMap.get(romanNumerals.charAt(romanNumerals.length() - 1));
-        }
-        System.out.println(sum);
+        String[] strings = {"ab", "a"};
+        System.out.println(longestCommonPrefix(strings));
 
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0){
+            return "";
+        }
+        boolean running = true;
+        String prefix = "";
+        boolean isPrefix = true;
+        int i = 0;
+        char curChar = 'T';
+        int smallestIndex = 0;
+        for(int j = 1; j < strs.length; j++){
+            if (strs[smallestIndex].length() > strs[j].length()){
+                    smallestIndex = j;
+            }
+        }
+        while(running){
+            if(strs[smallestIndex].length() == 0 || i > strs[smallestIndex].length() - 1){
+                running = false;
+                isPrefix = false;
+            }
+            else {
+                curChar = strs[0].charAt(i);
+            }
+            for(int j = 1; j < strs.length; j++){
+                if(i < strs[j].length() && strs[j].charAt(i) != curChar){
+                    isPrefix = false;
+                    running = false;
+                }
+            }
+            if(isPrefix){
+                prefix += curChar;
+            }
+            i++;
+        }
+        return prefix;
     }
 }
