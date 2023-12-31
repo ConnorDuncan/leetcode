57c427ba51217be588fc557947357125e3e961a9
Connor Duncan <connorduncan@cs.ucsb.edu>
2023-12-31 11:06:50 -0800

    (check if) Same Tree (fix extra curly brace)

--- src/helperObject.java
+++ src/helperObject.java
@@ -3,18 +3,15 @@ public class helperObject {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         return recursiveHelper(p, q);
     }
-    public boolean recursiveHelper(TreeNode p, TreeNode q){
-        if(p == null && q == null){
+    public boolean recursiveHelper(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
             return true;
-        }
-        else if(p == null || q == null){
+        } else if (p == null || q == null) {
             return false;
-        }
-        else if(p.val == q.val){
+        } else if (p.val == q.val) {
             return (recursiveHelper(p.right, q.right) && recursiveHelper(p.left, q.left));
         }
         return false;
-    }
     }
 }
 
