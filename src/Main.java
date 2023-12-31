af43086416c0e41b6c80e859f76969af48bc0358
Connor Duncan <connorduncan@cs.ucsb.edu>
2023-12-30 19:24:26 -0800

    Merge Sorted Array

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,40 @@ import java.lang.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        helperObject myDupeRemover = new helperObject();
-        ListNode head = new ListNode();
-        head.val = 1;
-        ListNode cur = head;
-        head.next = new ListNode(1);
-        cur = head.next;
-        cur.next = new ListNode(2);
-        cur = cur.next;
-        cur.next = new ListNode(2);
-        cur = cur.next;
+        int[] nums1 = new int[]{1, 2, 3, 0, 0, 0};
+        int[] nums2 = new int[]{2, 5, 6};
+        int m = 3;
+        int n = 3;
 
-        cur.next = new ListNode(3);
-        cur = cur.next;
-        cur.next = new ListNode(3);
-        head = myDupeRemover.deleteDuplicates(head);
-        while(head.next != null){
-            System.out.println(head.val);
-            head = head.next;
+        int[] mergedArray = new int[m + n];
+        int i = 0;
+        int j = 0;
+        while(i < m && j < n)
+        {
+            if(nums1[i] <= nums2[j]){
+                mergedArray[i + j] = nums1[i];
+                i++;
+            }
+            else{
+                mergedArray[i + j] = nums2[j];
+                j++;
+            }
         }
-        System.out.println(head.val);
-
+        while(i < m){
+            mergedArray[i + j] = nums1[i];
+            i++;
+        }
+        while(j < n){
+            mergedArray[i + j] = nums2[j];
+            j++;
+        }
+        nums1 = mergedArray;
+        System.out.print("[");
+        for(int k = 0; k < nums1.length - 1; k++){
+            System.out.print(nums1[k] + ", ");
+        }
+        System.out.print(nums1[nums1.length - 1]);
+        System.out.println("]");
     }
+
 }
