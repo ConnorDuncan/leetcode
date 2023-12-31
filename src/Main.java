39d16c08a7ed96a0dcc62e05cd3f5f88d11ea9b1
Connor Duncan <connorduncan@cs.ucsb.edu>
2023-12-30 17:22:30 -0800

    deleteDuplicates

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,26 @@ import java.lang.*;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        helperObject mySort = new helperObject();
+        helperObject myDupeRemover = new helperObject();
         ListNode head = new ListNode();
-        head.val = 4;
-        head.next = new ListNode(3);
-        ListNode output = mySort.sortList(head);
-        System.out.println(output.val);
-        System.out.println(output.next.val);
+        head.val = 1;
+        ListNode cur = head;
+        head.next = new ListNode(1);
+        cur = head.next;
+        cur.next = new ListNode(2);
+        cur = cur.next;
+        cur.next = new ListNode(2);
+        cur = cur.next;
+
+        cur.next = new ListNode(3);
+        cur = cur.next;
+        cur.next = new ListNode(3);
+        head = myDupeRemover.deleteDuplicates(head);
+        while(head.next != null){
+            System.out.println(head.val);
+            head = head.next;
+        }
+        System.out.println(head.val);
 
     }
 }
--- src/helperObject.java
+++ src/helperObject.java
@@ -1,28 +1,44 @@
 import java.util.*;
 public class helperObject {
-    public ListNode sortList(ListNode head) {
+    public ListNode deleteDuplicates(ListNode head) {
+        HashSet<Integer> myHash = new HashSet<Integer>();
         if(head == null){
             return null;
         }
         else if(head.next == null){
             return head;
         }
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        while(head.next != null){
-            list.add(head.val);
-            head = head.next;
+        ListNode cur = head.next;
+        ListNode prev = head;
+        myHash.add(head.val);
+        while(cur.next != null){
+            if(!myHash.contains(cur.val)){
+                myHash.add(cur.val);
+                prev = cur;
+                cur = cur.next;
+            }
+            else{
+                prev.next = cur.next;
+                cur = prev.next;
+            }
         }
-        list.add(head.val);
-        Collections.sort(list);
-        ListNode cur = new ListNode();
-        ListNode myHead = cur;
-        for(int i = 0; i < list.size() - 1; i++){
-            cur.val = list.get(i);
-            cur.next = new ListNode();
-            cur = cur.next;
+        if(myHash.contains(cur.val)){
+            prev.next = null;
         }
-        cur.val = list.get(list.size() - 1);
-        return myHead;
+        return head;
+/*
+More efficient solution:
+ListNode current = head;
+while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next; // Skip the duplicate node
+            } else {
+                current = current.next;
+            }
+}
+return head;
+ */
+
     }
 }
 
