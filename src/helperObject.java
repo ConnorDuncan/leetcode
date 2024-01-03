f4d356080c8a03ae93d2bc15f207a70177b54f74
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-03 15:47:59 -0800

    Is Anagram

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,25 +1,46 @@
 import java.util.*;
 public class helperObject {
-    public boolean isPowerOfTwo(int n) {
-        if(n == 1){
-            return true;
-        }
-        if(n < 0 || n % 2 == 1){
-            return false;
-        }
-        String binary = Integer.toBinaryString(n);
-        boolean seenOne = false;
-        for(int i = 0; i < binary.length(); i++){
-            if(binary.charAt(i) == '1'){
-                if(seenOne){
-                    return false;
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> mapS = new HashMap<Character, Integer>();
+        Map<Character, Integer> mapT = new HashMap<Character, Integer>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c != ' '){
+                if(mapS.containsKey(c)){
+                    mapS.put(c, mapS.get(c) + 1);
                 }
                 else{
-                    seenOne = true;
+                    mapS.put(c, 1);
                 }
             }
+
         }
-        return seenOne;
+        for(int i = 0; i < t.length(); i++){
+            char c = t.charAt(i);
+            if(c != ' '){
+                if(mapT.containsKey(c)){
+                    mapT.put(c, mapT.get(c) + 1);
+                }
+                else{
+                    mapT.put(c, 1);
+                }
+            }
+
+        }
+        for(char c : mapS.keySet()){
+            if(!mapT.containsKey(c)){
+                return false;
+            }
+            else if(!mapT.get(c).equals(mapS.get(c))){
+                return false;
+            }
+        }
+        for(char c : mapT.keySet()){
+            if(!mapS.containsKey(c)){
+                return false;
+            }
+        }
+        return true;
     }
 }
 
