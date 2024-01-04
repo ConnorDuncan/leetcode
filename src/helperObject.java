9519ba968916f720bff4a398b461d7b23b8cdbcb
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-03 21:19:08 -0800

    Hamming Weight (number of one bits)

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,40 +1,14 @@
 import java.util.*;
 public class helperObject {
-    public int minOperations(String s) {
-        int maxOne = 0;
-        int maxTwo = 0;
-        boolean alt = false;
-        for(int i = 0; i < s.length(); i++){
-            if(alt){
-                if(s.charAt(i) != '0'){
-                    maxOne++;
-                }
+    public int hammingWeight(int n) {
+        int sum = 0;
+        String myString = Integer.toBinaryString(n);
+        for(int i = 0; i < myString.length(); i++){
+            if(myString.charAt(i) == '1'){
+                sum++;
             }
-            else{
-                if(s.charAt(i) != '1'){
-                    maxOne++;
-                }
-            }
-            alt = !alt;
         }
-        alt = true;
-        for(int i = 0; i < s.length(); i++){
-            if(alt){
-                if(s.charAt(i) != '0'){
-                    maxTwo++;
-                }
-            }
-            else{
-                if(s.charAt(i) != '1'){
-                    maxTwo++;
-                }
-            }
-            alt = !alt;
-        }
-        if(maxOne > maxTwo){
-            return maxTwo;
-        }
-        return maxOne;
+        return sum;
     }
 
 }
