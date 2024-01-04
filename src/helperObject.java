f94a12bd128873f4ccca5070b62d9bd18fd4e09f
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-03 21:28:52 -0800

    numberOfMatches

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,17 +1,18 @@
 import java.util.*;
 public class helperObject {
-    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < word1.length; i++){
-            builder.append(word1[i]);
+    public int numberOfMatches(int n) {
+        int matches = 0;
+        while(n != 1){
+            if(n % 2 == 1){
+                matches += n / 2;
+                n = (n / 2) + 1;
+            }
+            else{
+                matches += n / 2;
+                n /= 2;
+            }
         }
-        String wordOne = builder.toString();
-        builder = new StringBuilder();
-        for(int i = 0; i < word2.length; i++){
-            builder.append(word2[i]);
-        }
-        String wordTwo = builder.toString();
-        return wordOne.equals(wordTwo);
+        return matches;
     }
 
 }
