bc2705aadef0d17b06fd472050aff764f5f47369
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-04 13:07:08 -0800

    Sum of Values at Indices with K bits

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,17 +1,21 @@
 import java.util.*;
 public class helperObject {
-    public String destCity(List<List<String>> paths) {
-        HashSet<String> mySet = new HashSet<String>();
-        int r = paths.size();
-        for(int i = 0; i < r; i++){
-            mySet.add(paths.get(i).get(0));
-        }
-        for(int i = 0; i < r; i++){
-            if(!mySet.contains(paths.get(i).get(1))){
-                return paths.get(i).get(1);
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int sum = 0;
+        for(int i = 0; i < nums.size(); i++){
+            String s = Integer.toBinaryString(i);
+            int numBits = 0;
+            for(int j = 0; j < s.length(); j++){
+                char c = s.charAt(j);
+                if(c == '1'){
+                    numBits++;
+                }
+            }
+            if(numBits == k){
+                sum += nums.get(i);
             }
         }
-        return "error";
+        return sum;
     }
 
 }
