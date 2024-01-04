d17134200ec93f27dd20226d47b97e1e7078d6be
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-03 19:07:00 -0800

    Find Content Children (Happy Children)

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,34 +1,22 @@
 import java.util.*;
 public class helperObject {
-    public int minOperations(int[] nums) {
-        HashMap<Integer, Integer> myMap = new HashMap<Integer, Integer>();
-        int count = 0;
-        for(int i : nums){
-            if(myMap.containsKey(i)){
-                myMap.put(i, myMap.get(i) + 1);
+    public int findContentChildren(int[] g, int[] s) {
+        int happyChildren = 0;
+        int j = 0;
+        int i = 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        while(j < g.length && i < s.length){
+            if(g[j] <= s[i]){
+                happyChildren++;
+                j++;
+                i++;
             }
             else{
-                myMap.put(i, 1);
-            }
-
-        }
-        for(int i : myMap.keySet()){
-            int val = myMap.get(i);
-            if(val == 1){
-                return -1;
-            }
-            while(val != 0){
-                if(val % 3 == 0 || (val != 2 && val != 4)){
-                    val -= 3;
-                    count++;
-                }
-                else if(val % 2 == 0){
-                    val -= 2;
-                    count++;
-                }
+                i++;
             }
         }
-        return count;
+        return happyChildren;
     }
 }
 
