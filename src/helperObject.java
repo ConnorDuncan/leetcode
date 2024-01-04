99f5c44869174b7b4ee97654c97cff7522e7f31d
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-03 16:14:44 -0800

    Add Binary

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,46 +1,46 @@
 import java.util.*;
 public class helperObject {
-    public boolean isAnagram(String s, String t) {
-        Map<Character, Integer> mapS = new HashMap<Character, Integer>();
-        Map<Character, Integer> mapT = new HashMap<Character, Integer>();
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(c != ' '){
-                if(mapS.containsKey(c)){
-                    mapS.put(c, mapS.get(c) + 1);
-                }
-                else{
-                    mapS.put(c, 1);
-                }
-            }
-
+    public String addBinary(String a, String b) {
+        StringBuilder myBuilder = new StringBuilder();
+        boolean remainder = false;
+        StringBuilder tempA = new StringBuilder(a);
+        StringBuilder tempB = new StringBuilder(b);
+        while(tempA.length() > tempB.length()){
+            tempB.insert(0, '0');
         }
-        for(int i = 0; i < t.length(); i++){
-            char c = t.charAt(i);
-            if(c != ' '){
-                if(mapT.containsKey(c)){
-                    mapT.put(c, mapT.get(c) + 1);
-                }
-                else{
-                    mapT.put(c, 1);
-                }
-            }
-
+        while(tempA.length() < tempB.length()){
+            tempA.insert(0, '0');
         }
-        for(char c : mapS.keySet()){
-            if(!mapT.containsKey(c)){
-                return false;
+        a = tempA.toString();
+        b = tempB.toString();
+        for(int i = a.length() - 1; i > -1; i--){
+            char aChar = a.charAt(i);
+            char bChar = b.charAt(i);
+            if(aChar == '0' && bChar == '0' && !remainder){
+                myBuilder.insert(0, '0');
             }
-            else if(!mapT.get(c).equals(mapS.get(c))){
-                return false;
+            else if(aChar == '0' && bChar == '0' && remainder){
+                myBuilder.insert(0, '1');
+                remainder = false;
             }
-        }
-        for(char c : mapT.keySet()){
-            if(!mapS.containsKey(c)){
-                return false;
+            else if((aChar == '0' && bChar == '1' && !remainder) || (aChar == '1' && bChar == '0' && !remainder)){
+                myBuilder.insert(0, '1');
+            }
+            else if((aChar == '0' && bChar == '1' && remainder) || (aChar == '1' && bChar == '0' && remainder)){
+                myBuilder.insert(0, '0');
+            }
+            else if(aChar == '1' && bChar == '1' && !remainder){
+                remainder = true;
+                myBuilder.insert(0, '0');
+            }
+            else{
+                myBuilder.insert(0, '1');
             }
         }
-        return true;
+        if(remainder){
+            myBuilder.insert(0, '1');
+        }
+        return myBuilder.toString();
     }
 }
 
