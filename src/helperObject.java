033f6a597f2789ee604cdcab3eb2af2b6f0228f3
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-03 21:24:35 -0800

    arrayStrings are equal

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,14 +1,17 @@
 import java.util.*;
 public class helperObject {
-    public int hammingWeight(int n) {
-        int sum = 0;
-        String myString = Integer.toBinaryString(n);
-        for(int i = 0; i < myString.length(); i++){
-            if(myString.charAt(i) == '1'){
-                sum++;
-            }
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder builder = new StringBuilder();
+        for(int i = 0; i < word1.length; i++){
+            builder.append(word1[i]);
         }
-        return sum;
+        String wordOne = builder.toString();
+        builder = new StringBuilder();
+        for(int i = 0; i < word2.length; i++){
+            builder.append(word2[i]);
+        }
+        String wordTwo = builder.toString();
+        return wordOne.equals(wordTwo);
     }
 
 }
