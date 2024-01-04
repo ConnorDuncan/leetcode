5574d63281678fdcc6db153d8e318b38f1f06b3e
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-04 11:19:44 -0800

    Element Appearing more than 25% of the time

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,15 +1,37 @@
 import java.util.*;
 public class helperObject {
-    public int[][] transpose(int[][] matrix) {
-        int r = matrix[0].length;
-        int c = matrix.length;
-        int[][] transpose = new int[r][c];
-        for(int i = 0; i < c; i++){
-            for(int j = 0; j < r; j++){
-                transpose[j][i] = matrix[i][j];
+    /* Original slower solution that finds max occurrence, regardless of that amount of occurrence and if sorted
+    public int findSpecialInteger(int[] arr) {
+        HashMap<Integer, Integer> myMap = new HashMap<Integer, Integer>();
+        for(int i = 0; i < arr.length; i++){
+            if(myMap.containsKey(arr[i])){
+                myMap.put(arr[i], myMap.get(arr[i]) + 1);
+            }
+            else{
+                myMap.put(arr[i], 1);
             }
         }
-        return transpose;
+        int max = 0;
+        int maxVal = 0;
+        for(int i : myMap.keySet()){
+            int temp = myMap.get(i);
+            if(temp > max){
+                max = temp;
+                maxVal = i;
+            }
+        }
+        return maxVal;
+    }
+     */
+
+    // New solution that only works for occurrence that is greater than 25% and if array is sorted
+    public int findSpecialInteger(int[] arr) {
+        for(int i = 0; i < arr.length - (arr.length / 4); i++){
+            if(arr[i] == arr[i + (arr.length / 4)]){
+                return arr[i];
+            }
+        }
+        return -1;
     }
 
 }
