f44125194a44c27aa7ee6660410440977162996e
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-05 14:23:57 -0800

    findTheDifference (between two strings)

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,17 +1,19 @@
 import java.util.*;
 public class helperObject {
-    public int firstUniqChar(String s) {
+    public char findTheDifference(String s, String t) {
         int[] count = new int[26];
-        int len = s.length();
-        for(int i = 0; i < len; i++){
+        for(int i = 0; i < s.length(); i++){
             count[s.charAt(i) - 97]++;
         }
-        for(int i = 0; i < len; i++){
-            if(count[s.charAt(i) - 97] == 1){
-                return i;
+        for(int i = 0; i < t.length(); i++){
+            count[t.charAt(i) - 97]--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(count[i] == -1){
+                return (char)(97 + i);
             }
         }
-        return -1;
+        return 'a';
     }
 }
 
