39c5ce6f781cc9d430b5a3feac2d91bcaed033e0
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-04 21:57:49 -0800

    postorder traversal

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class helperObject {
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> postorderTraversal(TreeNode root) {
         ArrayList<Integer> myArray = new ArrayList<Integer>();
         recursiveHelper(root, myArray);
         return myArray;
@@ -13,17 +13,17 @@ public class helperObject {
             myArray.add(root.val);
         }
         else if(root.left == null){
-            myArray.add(root.val);
             recursiveHelper(root.right, myArray);
+            myArray.add(root.val);
         }
         else if(root.right == null){
-            myArray.add(root.val);
             recursiveHelper(root.left, myArray);
+            myArray.add(root.val);
         }
         else{
-            myArray.add(root.val);
             recursiveHelper(root.left, myArray);
             recursiveHelper(root.right, myArray);
+            myArray.add(root.val);
         }
     }
 
