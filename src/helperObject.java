8ff6755c46c8d2bc7c182a0d0f24b567475880c2
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 11:39:04 -0800

    has alternating bits

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,36 +1,21 @@
 import java.util.*;
 public class helperObject {
-    public int calPoints(String[] operations) {
-        Stack<Integer> myStack = new Stack<Integer>();
-        for(int i = 0; i < operations.length; i++){
-            char c = operations[i].charAt(0);
-            if((c > 47 && c < 58) || c == 45){
-                int record = Integer.parseInt(operations[i]);
-                myStack.push(record);
-            }
-            else if(c == 43){
-                int record = 0;
-                int temp1 = myStack.pop();
-                int temp2 = myStack.pop();
-                record = temp1+temp2;
-                myStack.push(temp2);
-                myStack.push(temp1);
-                myStack.push(record);
-            }
-            else if(c == 68){
-                int score = myStack.pop();
-                myStack.push(score);
-                myStack.push(score*2);
-            }
-            else if(c == 67){
-                myStack.pop();
-            }
+    public boolean hasAlternatingBits(int n) {
+        String s = Integer.toBinaryString(n);
+        boolean firstIsOne = false;
+        if(s.charAt(0) == '1'){
+            firstIsOne = true;
         }
-        int sum = 0;
-        while(!myStack.empty()){
-            sum += myStack.pop();
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == '1' && firstIsOne){
+                return false;
+            }
+            else if(s.charAt(i) == '0' && !firstIsOne){
+                return false;
+            }
+            firstIsOne = !firstIsOne;
         }
-        return sum;
+        return true;
     }
 }
 
