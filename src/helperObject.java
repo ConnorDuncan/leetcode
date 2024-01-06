f62219653bf0d5dc27c9f710454265156c6c7446
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-05 23:57:20 -0800

    Check Record

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,42 +1,28 @@
 import java.util.*;
 public class helperObject {
-    public boolean detectCapitalUse(String word) {
-        int len = word.length();
-        if(len == 1){
-            return true;
-        }
-        boolean beginCapital = false;
-        if(word.charAt(0) > 64 && word.charAt(0) < 91){ // uppercase
-            beginCapital = true;
-        }
-        if(!beginCapital){
-            for(int i = 1; i < len; i++){
-                if(word.charAt(i) > 64 && word.charAt(i) < 91){
+    public boolean checkRecord(String s) {
+        int[] myInfo = new int[2]; // 0 = 'A' 1 = 'L'
+        int len = s.length();
+        for(int i = 0; i < len; i++){
+            char c = s.charAt(i);
+            if(c == 'A'){
+                myInfo[0]++;
+                myInfo[1] = 0;
+            }
+            else if(c == 'L'){
+                myInfo[1]++;
+                if(myInfo[1] >= 3){
                     return false;
                 }
             }
-            return true;
-        }
-        else{
-            boolean upper = false;
-            if(word.charAt(1) > 64 && word.charAt(1) < 91){
-                upper = true;
+            else{
+                myInfo[1] = 0;
             }
-            for(int i = 1; i < len; i++){
-                char c = word.charAt(i);
-                if(upper){
-                    if(c > 96 && c < 123){
-                        return false;
-                    }
-                }
-                else{
-                    if(c > 64 && c < 91){
-                        return false;
-                    }
-                }
-            }
-            return true;
         }
+        if(myInfo[0] > 1){
+            return false;
+        }
+        return true;
     }
 }
 
