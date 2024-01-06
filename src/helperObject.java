deea815445ebe84725e53f3df4b6d1903dcb6099
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 14:31:19 -0800

    flip and invert image (improve speed by saving lengths)

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,14 +1,16 @@
 import java.util.*;
 public class helperObject {
     public int[][] flipAndInvertImage(int[][] image) {
-        int[][] result = new int[image.length][image[0].length];
-        for(int i = 0; i < image.length; i++){
-            for(int j = 0; j < image[0].length; j++){
+        int l1 = image.length;
+        int l2 = image[0].length;
+        int[][] result = new int[l1][l2];
+        for(int i = 0; i < l1; i++){
+            for(int j = 0; j < l2; j++){
                 if(image[i][j] == 0){
-                    result[i][image[0].length - 1 - j] = 1;
+                    result[i][l2 - 1 - j] = 1;
                 }
                 else{
-                    result[i][image[0].length - 1 - j] = 0;
+                    result[i][l2 - 1 - j] = 0;
                 }
             }
         }
