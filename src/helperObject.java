d5a9a1fbfe70d75ab3b631e42fc713eb6c74b763
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 12:38:05 -0800

    next greatest letter

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,16 +1,12 @@
 import java.util.*;
 public class helperObject {
-    public String toLowerCase(String s) {
-        int len = s.length();
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < len; i++){
-            char c = s.charAt(i);
-            if(c >= 'A' && c <= 'Z'){
-                c += 32;
+    public char nextGreatestLetter(char[] letters, char target) {
+        for(int i = 0; i < letters.length; i++){
+            if(letters[i] > target){
+                return letters[i];
             }
-            builder.append(c);
         }
-        return builder.toString();
+        return letters[0];
     }
 }
 
