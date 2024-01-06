8f00aaa6b71c034dc129d5dd47f8a331d0cc4f5e
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 14:28:17 -0800

    flip and invert image

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,26 +1,18 @@
 import java.util.*;
 public class helperObject {
-    public String toGoatLatin(String sentence) {
-        StringBuilder builder = new StringBuilder();
-        String[] words = sentence.split(" ");
-        for(int i = 0; i < words.length; i++){
-            char c = words[i].charAt(0);
-            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
-                builder.append(words[i] + "ma");
-
-            }
-            else{
-                words[i] = words[i].substring(1) + c;
-                builder.append(words[i] + "ma");
-            }
-            for(int j = 0; j < i + 1; j++){
-                builder.append("a");
-            }
-            if(i != words.length - 1){
-                builder.append(" ");
+    public int[][] flipAndInvertImage(int[][] image) {
+        int[][] result = new int[image.length][image[0].length];
+        for(int i = 0; i < image.length; i++){
+            for(int j = 0; j < image[0].length; j++){
+                if(image[i][j] == 0){
+                    result[i][image[0].length - 1 - j] = 1;
+                }
+                else{
+                    result[i][image[0].length - 1 - j] = 0;
+                }
             }
         }
-        return builder.toString();
+        return result;
     }
 }
 
