535fa9b23f7ad4d40cf32a8216284468ce9cf48a
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 15:28:22 -0800

    uncommon words

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,41 +1,40 @@
 import java.util.*;
 public class helperObject {
-    public boolean lemonadeChange(int[] bills) {
-        int numFives = 0;
-        int numTens = 0;
-        int numTwentys = 0;
-        for(int i = 0; i < bills.length; i++){
-            if(bills[i] == 5){
-                numFives++;
-            }
-            else if(bills[i] == 10){
-                if(numFives < 1){
-                    return false;
-                }
-                numFives--;
-                numTens++;
+    public String[] uncommonFromSentences(String s1, String s2) {
+        HashMap<String, Integer> myMap = new HashMap<String, Integer>();
+        String[] words1 = s1.split(" ");
+        String[] words2 = s2.split(" ");
+        int len1 = words1.length;
+        int len2 = words2.length;
+        for(int i = 0; i < len1; i++){
+            if(myMap.containsKey(words1[i])){
+                myMap.put(words1[i], myMap.get(words1[i]) + 1);
             }
             else{
-                if(numFives < 1){
-                    return false;
-                }
-                else if(numFives == 1 && numTens < 1){
-                    return false;
-                }
-                else if(numFives == 2 && numTens < 1){
-                    return false;
-                }
-                if(numTens >= 1){
-                    numTens--;
-                    numFives--;
-                }
-                else{
-                    numFives -= 3;
-                }
-                numTwentys++;
+                myMap.put(words1[i], 1);
             }
         }
-        return true;
+        for(int i = 0; i < len2; i++){
+            if(myMap.containsKey(words2[i])){
+                myMap.put(words2[i], myMap.get(words2[i]) + 1);
+            }
+            else{
+                myMap.put(words2[i], 1);
+            }
+        }
+        ArrayList<String> myArrayList = new ArrayList<String>();
+        for(String s : myMap.keySet()){
+            if(myMap.get(s) == 1){
+                myArrayList.add(s);
+            }
+        }
+        String[] finalArr = new String[myArrayList.size()];
+        int i = 0;
+        for(String s : myArrayList){
+            finalArr[i] = s;
+            i++;
+        }
+        return finalArr;
     }
 }
 
