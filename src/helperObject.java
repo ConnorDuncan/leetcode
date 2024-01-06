2a113a8ee6d6c35dd058955284474fc954c90dd4
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 13:27:22 -0800

    num jewels in stones

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,43 +1,17 @@
 import java.util.*;
 public class helperObject {
-    public String shortestCompletingWord(String licensePlate, String[] words) {
-        char[] letters = new char[26];
-        int len = licensePlate.length();
-        int shortestLen = 16;
-        int shortestIndex = 0;
-        for(int i = 0; i < len; i++){
-            char c = licensePlate.charAt(i);
-            if(c >= 'a' && c <= 'z'){
-                letters[c - 97]++;
-            }
-            else if(c >= 'A' && c <= 'Z'){
-                letters[c - 65]++;
+    public int numJewelsInStones(String jewels, String stones) {
+        HashSet<Character> mySet = new HashSet<Character>();
+        int total = 0;
+        for(int i = 0; i < jewels.length(); i++){
+            mySet.add(jewels.charAt(i));
+        }
+        for(int i = 0; i < stones.length(); i++){
+            if(mySet.contains(stones.charAt(i))){
+                total++;
             }
         }
-        len = words.length;
-        for(int i = 0; i < len; i++){ // for each word
-            char[] lettersW = new char[26]; // make array to count characters
-            for(int j = 0; j < words[i].length(); j++){ // for each character in word
-                char c = words[i].charAt(j);
-                if(c >= 'a' && c <= 'z'){
-                    lettersW[c - 97]++;                 // count the character
-                }
-            }
-            boolean valid = true;           // assume the word is valid
-            for(int j = 0; j < 26; j++){    // for each character in alphabet
-                if(letters[j] > lettersW[j]){   // if the license plate has more letters than letters in the word
-                    valid = false;
-                }
-            }
-            if(valid){
-                int wLen = words[i].length();
-                if(wLen < shortestLen){
-                    shortestLen = wLen;
-                    shortestIndex = i;
-                }
-            }
-        }
-        return words[shortestIndex];
+        return total;
     }
 }
 
