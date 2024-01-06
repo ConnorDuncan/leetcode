b5352fcd6a0f3840aafea6badb445bd95ac6cedd
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 13:06:02 -0800

    shortest completing word

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         helperObject myObj = new helperObject();
+        String license = "1s3 PSt";
+        String[] words = new String[]{"step","steps"};
+        String result = myObj.shortestCompletingWord(license, words);
+        System.out.println(result);
     }
 
 }
--- src/helperObject.java
+++ src/helperObject.java
@@ -1,22 +1,43 @@
 import java.util.*;
 public class helperObject {
-    public int dominantIndex(int[] nums) {
-        int max = -1;
-        int index = -1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > max){
-                max = nums[i];
-                index = i;
+    public String shortestCompletingWord(String licensePlate, String[] words) {
+        char[] letters = new char[26];
+        int len = licensePlate.length();
+        int shortestLen = 16;
+        int shortestIndex = 0;
+        for(int i = 0; i < len; i++){
+            char c = licensePlate.charAt(i);
+            if(c >= 'a' && c <= 'z'){
+                letters[c - 97]++;
+            }
+            else if(c >= 'A' && c <= 'Z'){
+                letters[c - 65]++;
             }
         }
-        for(int i = 0; i < nums.length; i++){
-            if(i != index){
-                if(2 * nums[i] > nums[index]){
-                    return -1;
+        len = words.length;
+        for(int i = 0; i < len; i++){ // for each word
+            char[] lettersW = new char[26]; // make array to count characters
+            for(int j = 0; j < words[i].length(); j++){ // for each character in word
+                char c = words[i].charAt(j);
+                if(c >= 'a' && c <= 'z'){
+                    lettersW[c - 97]++;                 // count the character
+                }
+            }
+            boolean valid = true;           // assume the word is valid
+            for(int j = 0; j < 26; j++){    // for each character in alphabet
+                if(letters[j] > lettersW[j]){   // if the license plate has more letters than letters in the word
+                    valid = false;
+                }
+            }
+            if(valid){
+                int wLen = words[i].length();
+                if(wLen < shortestLen){
+                    shortestLen = wLen;
+                    shortestIndex = i;
                 }
             }
         }
-        return index;
+        return words[shortestIndex];
     }
 }
 
