be811054e93b2b91c591a38546e4a6cc2957f8e1
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-05 23:49:28 -0800

    Detect Capital

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,22 +1,42 @@
 import java.util.*;
 public class helperObject {
-    public List<String> fizzBuzz(int n) {
-        ArrayList<String> myList = new ArrayList<String>();
-        for(int i = 1; i <= n; i++){
-            if(i % 3 == 0 && i % 5 == 0){
-                myList.add("FizzBuzz");
-            }
-            else if(i % 3 == 0){
-                myList.add("Fizz");
-            }
-            else if(i % 5 == 0){
-                myList.add("Buzz");
-            }
-            else{
-                myList.add("" + i);
-            }
+    public boolean detectCapitalUse(String word) {
+        int len = word.length();
+        if(len == 1){
+            return true;
         }
-        return myList;
+        boolean beginCapital = false;
+        if(word.charAt(0) > 64 && word.charAt(0) < 91){ // uppercase
+            beginCapital = true;
+        }
+        if(!beginCapital){
+            for(int i = 1; i < len; i++){
+                if(word.charAt(i) > 64 && word.charAt(i) < 91){
+                    return false;
+                }
+            }
+            return true;
+        }
+        else{
+            boolean upper = false;
+            if(word.charAt(1) > 64 && word.charAt(1) < 91){
+                upper = true;
+            }
+            for(int i = 1; i < len; i++){
+                char c = word.charAt(i);
+                if(upper){
+                    if(c > 96 && c < 123){
+                        return false;
+                    }
+                }
+                else{
+                    if(c > 64 && c < 91){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
     }
 }
 
