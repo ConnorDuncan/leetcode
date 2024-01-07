da36ef0ade756cbdd8618b8c4ff4ac6d4a5bca79
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 20:48:29 -0800

    largest good integer

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,13 +1,37 @@
 import java.util.*;
 public class helperObject {
-    public int[] sortedSquares(int[] nums) {
-        int len = nums.length;
-        int[] returnarr = new int[len];
-        for(int i = 0; i < len; i++){
-            returnarr[i] = nums[i] * nums[i];
+    public String largestGoodInteger(String num) {
+        if(num.contains("999")){
+            return "999";
         }
-        Arrays.sort(returnarr);
-        return returnarr;
+        else if(num.contains("888")){
+            return "888";
+        }
+        else if(num.contains("777")){
+            return "777";
+        }
+        else if(num.contains("666")){
+            return "666";
+        }
+        else if(num.contains("555")){
+            return "555";
+        }
+        else if(num.contains("444")){
+            return "444";
+        }
+        else if(num.contains("333")){
+            return "333";
+        }
+        else if(num.contains("222")){
+            return "222";
+        }
+        else if(num.contains("111")){
+            return "111";
+        }
+        else if(num.contains("000")){
+            return "000";
+        }
+        return "";
     }
 }
 
