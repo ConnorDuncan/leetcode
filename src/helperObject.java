7b69b68d8e9944d7a36a5de94f20ee040bcc77c9
Connor Duncan <connorduncan@cs.ucsb.edu>
2024-01-06 17:10:41 -0800

    number valid emails

--- src/helperObject.java
+++ src/helperObject.java
@@ -1,25 +1,32 @@
 import java.util.*;
 public class helperObject {
-    public String reverseOnlyLetters(String s) {
-        Stack<Character> myStack = new Stack<Character>();
-        StringBuilder builder = new StringBuilder();
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)){
-                myStack.push(c);
-            }
-        }
-        for(int i = 0; i < s.length(); i++){
-            char c = s.charAt(i);
-            if(!(c >= 65 && c <= 90) && !(c >= 97 && c <= 122)){
-                builder.append(c);
-            }
-            else{
-                builder.append(myStack.pop());
-            }
-        }
-        return builder.toString();
+    public int numUniqueEmails(String[] emails) {
+        HashSet<String> validEmails = new HashSet<String>();
+        for(String s : emails){
+            String[] splitArr = s.split("@"); // 1 is domain, 0 is local name
+            boolean reading = true;
+            StringBuilder builder = new StringBuilder();
+            for(int i = 0; i < splitArr[0].length(); i++){
+                if(reading){
+                    char c = splitArr[0].charAt(i);
+                    if(c == '.'){
 
+                    }
+                    else if(c == '+'){
+                        reading = false;
+                    }
+                    else{ //alphabet character
+                        builder.append(c);
+                    }
+                }
+            }
+            validEmails.add(builder.toString() + "@" + splitArr[1]);
+        }
+        int sum = 0;
+        for(String s : validEmails){
+            sum++;
+        }
+        return sum;
     }
 }
 
